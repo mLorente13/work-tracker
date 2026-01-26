53aeb2f24ccc8a0dc9d0e0d2023419e736aa1a09
Marc Lorente <mlorente2005@gmail.com>
2026-01-26 16:35:29 +0100

    fix: Ensure refreshing state is reset after fetching workdays

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -25,6 +25,7 @@ export default function HomeScreen() {
         setWorkDays(data || []);
         console.log("Fetched workdays:", data);
       }
+      setRefreshing(false);
     };
     getUserWorkDays();
   }, [session, refreshing]);
